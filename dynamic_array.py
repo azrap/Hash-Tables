398d0857b8ac4ad27788d230a076254f56e48b98
azrap <azraap@gmail.com>
2019-12-26 21:22:21 -0800

    adding finishing touches to the insert

--- dynamic_array.py
+++ dynamic_array.py
@@ -8,9 +8,22 @@ class DynamicArray:
         if self.count >= self.capacity:
             # TODO: make array resize
             print("ERROR: Array is full")
+            return
         # shift everything at index to the right
+        if index > self.count:
+            print("ERROR: Out of range")
+            return
         for i in range(self.count, index, -1):
             self.storage[i] = self.storage[i-1]
 
         self.storage[index] = value
         self.count += 1
+
+
+my_array = DynamicArray(3)
+my_array.insert(10, 3)
+my_array.insert(0, 5)
+my_array.insert(0, 4)
+my_array.insert(0, 3)
+
+print(my_array.storage)
